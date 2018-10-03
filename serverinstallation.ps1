ea60df0b94329b56d54569447633914a3ddc7a5c
JoeWetmore <joew@intervision.com>
2018-10-03 12:24:26 -0700

    Server Installation Command

---
+++ serverinstallation.ps1
@@ -0,0 +1,2 @@
+./setup.exe /mode:install /IAcceptExchangeServerLicenseTerms /Role:mailbox /customerfeedbackenabled:false /targetdir:"D:\\Exchange\\" /MdbName:STL-AB /dbfilepath:"M:\\ExchangeDB\\STL-AB\\STL-AB.edb" /LogFolderPath:"L:\\ExchangeLogs\\STL-AB\\" 
+
